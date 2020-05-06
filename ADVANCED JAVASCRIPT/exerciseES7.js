e16c957581971d3ef5347eed04bd11a5708e33d6
colin richards <colin.rhys.richards@gmail.com>
2020-05-06 14:57:08 -0600

    js ex 8

--- ADVANCED JAVASCRIPT/exerciseES7.js
+++ ADVANCED JAVASCRIPT/exerciseES7.js
@@ -4,11 +4,15 @@
 // #1) Check if this array includes the name "John".
 const dragons = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
 
-
+dragons.includes('John')
 // #2) Check if this array includes any name that has "John" inside of it. If it does, return that
 // name or names in an array.
-const dragons = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
 
+//not the best way. Use filter
+// const dragons = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
+// if(dragons.includes('John')){
+// alert(dragons)
+// } else alert("it doesn't contain John")
 
 // #3) Create a function that calulates the power of 100 of a number entered as a parameter
 
